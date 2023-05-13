323f3388475876b8ec7c50081e4a5c6faec0b60e
Reginaldo Campelo <regin.developer@gmail.com>
2023-05-13 13:31:22 -0300

    adding personal info class

---
+++ src/app/shared/models/personal-info.ts
@@ -0,0 +1,9 @@
+export class PersonalInfo {
+  constructor(
+      public name: string,
+      public lastName: string,
+      public email: string,
+      public phone: string,
+      public linkedin?: string,
+  ) {}
+}
