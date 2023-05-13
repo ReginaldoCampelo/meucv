973839f472c6ddc5c6b151fe2efd55fa363c7f20
Reginaldo Campelo <regin.developer@gmail.com>
2023-05-13 13:23:41 -0300

    adding new cv component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './pages/home/home.component';
+import { NewCvComponent } from './pages/new-cv/new-cv.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent
+    HomeComponent,
+    NewCvComponent
   ],
   imports: [
     BrowserModule,
